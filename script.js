47929d1043b44f7179af1ba82ceb95043757cfdd
skhandelwal758 <65276353+skhandelwal758@users.noreply.github.com>
2023-02-24 16:22:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let textbox = document.getElementById("evaluatedText");
 textbox.addEventListener('input', function(){
     var text = this.value;
+	text = text.trim();
     // console.log(text);
     let words = text.split(" ");
     let len = words.length;
