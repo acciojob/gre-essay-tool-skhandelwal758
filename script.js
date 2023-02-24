f3449db0bf95b2d422253f2dd8799282bc30eef5
skhandelwal758 <65276353+skhandelwal758@users.noreply.github.com>
2023-02-24 16:18:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+let textbox = document.getElementById("evaluatedText");
+textbox.addEventListener('input', function(){
+    var text = this.value;
+    // console.log(text);
+    let words = text.split(" ");
+    let len = words.length;
+    document.getElementById("wordCount").innerText = len;
+    console.log()
+})
