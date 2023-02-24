3737e1212474541f5cd8707878a4a0e3f2dd9e01
skhandelwal758 <65276353+skhandelwal758@users.noreply.github.com>
2023-02-24 16:33:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ textbox.addEventListener('input', function(){
     // console.log(text);
     let words = text.split(" ");
 	let newArray = words.filter(function(ele){
-		return ele != " "; 
+		return ele != ""; 
 	});
     let len = newArray.length;
     document.getElementById("wordCount").innerText = len;
-    console.log()
+
 })
